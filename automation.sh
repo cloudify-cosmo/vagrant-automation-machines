dbcf01d6d7480cdb3aad3581bb7a05a3ded06092
guym <guym@gigaspaces.com>
2014-10-27 08:26:44 +0200

    add documentation on how to handle encryption keys and the use for hooks before vagrant runs and after it runs.

---
+++ automation.sh
@@ -0,0 +1,22 @@
+## a generic script for automation
+
+
+## pre vagrant hook
+HOOK_FILE=pre_vagrant_hook.sh
+if [ -f $HOOK_FILE ]
+   chmod 755 $HOOK_FILE
+   source $HOOK_FILE
+fi
+
+pushd $CLOUD_FOLDER
+vagrant up --provider $VAGRANT_PROVIDER
+vagrant rsync
+vagrant provision
+vagrant destroy
+
+HOOK_FILE=post_vagrant_hook.sh
+if [ -f $HOOK_FILE ]
+   chmod 755 $HOOK_FILE
+   source $HOOK_FILE
+fi
+
